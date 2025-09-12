2cc068d33b7734cb7501794e45dd80053fe4f7b0
Emilio Bensignor <liobensignor@gmail.com>
2025-09-12 10:24:33 -0300

    Fix Imagen Hero

--- app/components/home/Hero.vue
+++ app/components/home/Hero.vue
@@ -1,18 +1,21 @@
 <template>
     <DefaultSection class="relative">
         <picture class="w-full">
-            <source media="(min-width: 1440px)" srcset="/images/home/hero/Tzadik-Maquinaria-Agricola-Vial-DesktopXL.webp">
+            <source media="(min-width: 1440px)"
+                srcset="/images/home/hero/Tzadik-Maquinaria-Agricola-Vial-DesktopXL.webp">
             <source media="(min-width: 1080px)" srcset="/images/home/hero/Tzadik-Maquinaria-Agricola-Vial-Desktop.webp">
             <source media="(min-width: 768px)" srcset="/images/home/hero/Tzadik-Maquinaria-Agricola-Vial-Tablet.webp">
-            <img src="/images/home/hero/Tzadik-Maquinaria-Agricola-Vial-Mobile.webp" alt="Tzadik Maquinaria Agricola Vial"
+            <img src="/images/home/hero/Tzadik-Maquinaria-Agricola-Vial-Mobile.webp"
+                alt="Tzadik Maquinaria Agricola Vial"
                 class="w-full h-[28rem] md:h-80 lg:h-[25.5rem] xxl:h-[33.25rem] object-cover object-left">
         </picture>
-        <div class="w-full h-full flex flex-col items-center justify-center lg:justify-start gap-3 lg:gap-4 absolute text-light p-5 lg:pt-20 xxl:pt-[7.5rem]"
-            style="background: linear-gradient(119.89deg, rgba(239, 224, 219, 0.8) -31.4%, rgba(53, 38, 49, 0.8) 36.22%);">
+        <div
+            class="w-full h-full flex flex-col items-center justify-center lg:justify-start gap-3 lg:gap-4 absolute text-light p-5 lg:pt-20 xxl:pt-[7.5rem]">
             <HeadingH1 class="md:max-w-[27.75rem] lg:max-w-[35rem] xxl:max-w-[42rem]">
                 MAQUINARIA AGRÍCOLA Y VÍAL PARA MAXIMIZAR TU PRODUCTIVIDAD
             </HeadingH1>
-            <p class="md:max-w-[27.75rem] lg:max-w-[35rem] xxl:max-w-[42rem] text-center text-sm md:text-base lg:text-xl xxl:text-2xl font-medium">
+            <p
+                class="md:max-w-[27.75rem] lg:max-w-[35rem] xxl:max-w-[42rem] text-center text-sm md:text-base lg:text-xl xxl:text-2xl font-medium">
                 Para que tu trabajo no se detenga: asistencia técnica, repuestos y mantenimiento.
             </p>
         </div>
